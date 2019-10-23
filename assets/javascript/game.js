a498cf639342aac2b477b1c033263c4dbd2262c0
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-23 12:34:03 -0500

    added question object array and filled in data structure, removed extra images

---
+++ assets/javascript/game.js
@@ -0,0 +1,122 @@
+const questions = {[
+    Question0 = {
+        question = "Will smith drives a futuristic concept of this car in I.Robot.",
+        image = "./assets/images/audi r8 emblem",
+        wrongAnswers = {
+            A = "Honda",
+            B = "Porsche",
+            C = "Dodge",
+        },
+        correctAnswer = {
+            D = "Audi",
+        },
+    },
+    Question1 = {
+        question = "This is a famous british car company based out of Crewe.",
+        image = "./assets/images/bentley emblem.jpg",
+        wrongAnswers = {
+            A = "BMW",
+            B = "Mini Cooper",
+            C = "Land Rover",
+        },
+        correctAnswer = {
+            D = "Bentley",
+        }
+    },
+    Question2 = {
+        question = "This sub brand is famous for naming a model of car after an aquatic species.",
+        image = "./assets/images/chevy corvette emblem.jpg",
+        wrongAnswers = {
+            A = "Mopar",
+            B = "AMG",
+            C = "V-Tec",
+        },
+        correctAnswer = {
+            D = "Corvette",
+        }
+    },
+    Question3 = {
+        question = "This car has a large following in the low-rider community.",
+        image = "./assets/images/chevy impala emblem.jpg",
+        wrongAnswers = {
+            A = "Dodge",
+            B = "Ford",
+            C = "Pontiac",
+        },
+        correctAnswer = {
+            D = "Chevy",
+        }
+    },
+    Question4 = {
+        question = "This car maker is famous for this series having keys color coded to unlock different performance specs of the car.",
+        image = "./assets/images/dodge srt hellcat emblem.jpg",
+        wrongAnswers = {
+            A = "Nissan",
+            B = "BMW",
+            C = "Mclaren",
+        },
+        correctAnswer = {
+            D = "Dodge",
+        }
+    },
+    Question5 = {
+        question = "This brand came from Alfa Romeo's race division in 1939.",
+        image = "./assets/images/ferrari emblem.jpg",
+        wrongAnswers = {
+            A = "Alfa Romeo",
+            B = "Lamborghini",
+            C = "Opal",
+        },
+        correctAnswer = {
+            D = "Ferrari",
+        }
+    },
+    Question6 = {
+        question = "In 2008 Tata motors purchased this company from Ford.",
+        image = "./assets/images/jaguar emblem.jpg",
+        wrongAnswers = {
+            A = "Land Rover",
+            B = "Ford",
+            C = "Corvette",
+        },
+        correctAnswer = {
+            D = "Jaguar",
+        }
+    },
+    Question7 = {
+        question = "This brand is the largest luxury vehicle seller in the world.",
+        image = "./assets/mercedes emblem.jpg",
+        wrongAnswers = {
+            A = "Buick",
+            B = "BMW",
+            C = "Volkswagen",
+        },
+        correctAnswer = {
+            D = "Mercedes",
+        }
+    },
+    Question8 = {
+        question = "This brand also makes planes.",
+        image = "./assets/images/rolls royce emblem.jpg",
+        wrongAnswers = {
+            A = "Renault",
+            B = "Koenisegg",
+            C = "Lamborghini",
+        },
+        correctAnswer = {
+            D = "Rolls Royce",
+        }
+    },
+    Question9 = {
+        question = "This brand also makes planes.",
+        image = "./assets/images/saab emblem.jpg",
+        wrongAnswers = {
+            A = "BMW",
+            B = "Ferrari",
+            C = "Volkswagen",
+        },
+        correctAnswer = {
+            D = "SAAB",
+        }
+    },
+]}
